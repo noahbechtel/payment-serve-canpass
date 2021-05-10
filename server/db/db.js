6537a621d3d617ada2ef66d051157cd2878f3c97
Noah <noahbehtel@gmail.com>
2021-05-09 23:38:58 -0500

    dev

--- server/db/db.js
+++ server/db/db.js
@@ -6,13 +6,20 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    logging: false,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: process.env.DATABASE_URL
+        ? {
+            require: true,
+            rejectUnauthorized: false
+          }
+        : null
+    }
   }
 )
 module.exports = db
 
-// This is a global Mocha hook used for resource cleanup.
-// Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
   after('close database connection', () => db.close())
 }
