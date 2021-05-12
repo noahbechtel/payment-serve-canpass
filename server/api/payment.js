87da0ab299ad361825ca02f31eca31c2112cd78b
Noah <noahbehtel@gmail.com>
2021-05-12 09:17:01 -0500

    Test points

---
+++ server/api/payment.js
@@ -0,0 +1,52 @@
+const router = require('express').Router()
+const stripe = require('stripe')(
+  'sk_test_51Hunp9DchL2cGl0JQw105uqYbZztY8Xdga5yzjyfSljGPIe5ksNfLx2GLnOj9SY7kOY87U2qSVnP1gBSx3QmUs6N00FtU7mnCn'
+)
+module.exports = router
+
+router.post('/charge', async (req, res, next) => {
+  try {
+    const {amountDue, token} = req.body
+    const charge = await stripe.charges.create({
+      amount: amountDue,
+      currency: 'usd',
+      description: `Test Charge`,
+      source: token.id
+    })
+    res.json(charge)
+  } catch (err) {
+    next(err)
+  }
+})
+router.post('/createCustomer', async (req, res, next) => {
+  try {
+    const {email} = req.body
+    const customer = await stripe.customer.create({
+      email
+    })
+    res.json(customer)
+  } catch (err) {
+    next(err)
+  }
+})
+router.post('/createCard', async (req, res, next) => {
+  try {
+    const {customerId, cardToken} = req.body
+    const card = await stripe.customers.createSource(customerId, {
+      source: cardToken
+    })
+    res.json(card)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/getCustomer', async (req, res, next) => {
+  try {
+    const {customerId} = req.body
+    const customer = await stripe.customer.retrieve(customerId)
+    res.json(customer)
+  } catch (err) {
+    next(err)
+  }
+})
