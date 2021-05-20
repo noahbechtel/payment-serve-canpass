c27e3fa9fa3402ef5dfb73af1772859e48b8bd69
Noah <noahbehtel@gmail.com>
2021-05-20 13:48:24 -0500

    frontend

--- client/routes.js
+++ client/routes.js
@@ -3,19 +3,58 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {me} from './store'
+import StripeCheckout from 'react-stripe-checkout'
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData()
+  constructor() {
+    super()
+    this.state = {
+      id: '',
+      card: '',
+      email: '',
+      client_ip: '',
+      brand: '',
+      country: '',
+      last4: ''
+    }
   }
-
+  onToken = token => {
+    const {id, email, client_ip} = token
+    const {brand, country, last4} = token.card
+    const card = token.card.id
+    this.setState({id, card, email, client_ip, brand, country, last4})
+  }
   render() {
-    const {isLoggedIn} = this.props
-
-    return <div />
+    const {id, card, email, client_ip, brand, country, last4} = this.state
+    return (
+      <div>
+        {id !== '' ? (
+          <div>
+            <p>Payment token: {id}</p>
+            <p>Email: {email}</p>
+            <p>Client Country: {country}</p>
+            <p>Client IP: {client_ip}</p>
+            <p>Card token: {card}</p>
+            <p>Card Issuer: {brand}</p>
+            <p>Card Last Four: {last4}</p>
+          </div>
+        ) : null}
+        <StripeCheckout
+          amount={100.0}
+          // billingAddress
+          description="Test"
+          image={null}
+          locale="auto"
+          stripeKey="pk_test_51Hunp9DchL2cGl0JPXE4R1onCDCeHFL1HZuIZH2wXycdaeKIbttFN322uSKGAx0eGJmc0RYRp1JRQV795pvCqSje00wV7c1Se9"
+          token={this.onToken}
+          label="Pay with 💳"
+          panelLabel="Test Payment"
+        />
+      </div>
+    )
   }
 }
 
